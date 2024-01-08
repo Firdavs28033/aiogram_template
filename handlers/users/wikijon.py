ee8fd59a5665f0c84478560287af44b4cb514a30
Firdavs <burgutlochin11@gmail.com>
2024-01-08 09:18:35 +0500

    bot

---
+++ handlers/users/wikijon.py
@@ -0,0 +1,20 @@
+from aiogram import types
+import wikipedia
+from loader import dp
+
+wikipedia.set_lang('uz')
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Wikipeida Botiga Xush Kelibsiz!")
+
+@dp.message_handler()
+async def sendWiki(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bu mavzuga oid maqola topilmadi")
